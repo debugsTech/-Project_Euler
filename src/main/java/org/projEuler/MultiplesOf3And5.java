daec93f29a3626475b4b330cb59a941eb2eb862d
debugsTech <sal@debugs.tech>
2024-08-13 23:31:57 -0700

    Replaced System.out with Logger for output

--- src/main/java/org/projEuler/MultiplesOf3And5.java
+++ src/main/java/org/projEuler/MultiplesOf3And5.java
@@ -1,11 +1,15 @@
 package org.projEuler;
 
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9.
 The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000. */
-public class multiplesOf3And5 {
+public class MultiplesOf3And5 {
+    static final Logger logger = Logger.getLogger(MultiplesOf3And5.class.getName());
     public static void main(String[] args0) {
         int output = 0;
         for (int i = 1; i <= 999; i++) {
@@ -13,6 +17,6 @@ public class multiplesOf3And5 {
                 output += i;
             }
         }
-        System.out.println(output);
+        logger.log(Level.INFO, "The sum of all multiples of 3 or 5 below 1000 is: {0}", output);
     }
 }
